18a4102a90082a74287a4b66df1010957249b8a0
avergnaud <a.vergnaud@gmail.com>
2019-06-25 15:54:37 +0200

    Updates

---
+++ precache-manifest.40424b61c998a3804eaa6d0911dec560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1ac2dc19f3116757f42fea5d9eb8f6",
+    "url": "/qr-react/index.html"
+  },
+  {
+    "revision": "57075323ffae150e2bc3",
+    "url": "/qr-react/static/css/main.2c41fa61.chunk.css"
+  },
+  {
+    "revision": "c4db6c31ef4300378273",
+    "url": "/qr-react/static/js/2.853fad5e.chunk.js"
+  },
+  {
+    "revision": "57075323ffae150e2bc3",
+    "url": "/qr-react/static/js/main.351d4a30.chunk.js"
+  },
+  {
+    "revision": "151aa696a3a5dc4b751a",
+    "url": "/qr-react/static/js/runtime~main.10b53de8.js"
+  }
+]);
